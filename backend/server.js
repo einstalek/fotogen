d43723e70bca71db325254f5a454a36f46923ecb
einstalek <arganaidi@phystech.edu>
2025-03-03 12:22:06 +0200

    change port for server

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ const { v4: uuidv4 } = require('uuid');
 dotenv.config();
 
 const app = express();
-const PORT = 3001;
+// const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
@@ -122,6 +123,6 @@ app.get('/api/check-runpod-job/:id', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
